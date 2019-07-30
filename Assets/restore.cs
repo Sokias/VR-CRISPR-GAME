b791cf189db73c21fc22ef0c76576eb4afa26c00
Felix <573452881@qq.com>
2019-07-30 13:39:58 +0100

    20.39

---
+++ Assets/restore.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class restore : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Vector3 v;
+
+    void Start()
+    {
+        v = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K)) {
+            transform.position = v;
+        }
+    }
+}
